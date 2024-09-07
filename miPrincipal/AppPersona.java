af827d21a446815da92fd49e430b7cc5c3d1ecf9
Ruiz Elizalde Misael <L23170055@culiacan.tecnm.mx>
2024-09-07 05:03:01 +0000

    completo 06-09I

--- miPrincipal/AppPersona.java
+++ miPrincipal/AppPersona.java
@@ -4,7 +4,7 @@ public class AppPersona{
         System.out.println("==================================");
 		System.out.println("    Operaciones con Personas      ");
         System.out.println("==================================");
-        Persona p1 = new Persona("AELL680111","Lourdes Armenta",new Fecha("11/2/1968"));
+        Persona p1 = new Persona("AELL777711","Misael Ruiz",new Fecha("17/05/2005"));
         System.out.println(p1.getDni());
         System.out.println(p1.getNombre());
         System.out.println(p1.getFechaNacimiento());
